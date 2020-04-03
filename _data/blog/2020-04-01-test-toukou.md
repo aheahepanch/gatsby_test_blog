9cf02e02f8b7a9566f4b8a36bc4df535ad98051b
aheahepanch <hokazonotakahiro@aheahe-pc-mac.local>
2020-04-03 16:18:46 +0900

    create repogitry

---
+++ _data/blog/2020-04-01-test-toukou.md
@@ -0,0 +1,13 @@
+---
+template: BlogPost
+path: /test-toukou
+date: 2020-04-01T16:17:18.332Z
+title: テスト投稿
+thumbnail: /assets/curemilkey_01.jpg
+metaDescription: Meta 00000001
+---
+キュアミルキーかわいいよキュアミルキー
+
+スタートゥインクル☆プリキュア</br>
+あへあへ単打マン
+![vuremilkey](/assets/curemilkey_01.jpg)
--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Delog`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `ahead-test-blog`,
+    description: `あああ`,
     siteUrl: `https://delog-w3layouts.netlify.com/`,
     home: {
-      title: `Hi! I'm Delog`,
-      description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
+      title: `お勉強のために作ったブログ`,
+      description: `お勉強のために作ったキュアミルキーを讃えるためのブログ`,
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
     w3l_dom_key: `5e609f7a2d23fCF_Domain_verify` 
